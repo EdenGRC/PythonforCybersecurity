92a665985c796a50783368a8f65d3b171fba8e63
Steveede000 <steveede000@gmail.com>
2024-10-06 23:51:40 -0700

    New script to ask name and send a good message.

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+#create a script that takes user inserted name and outputs a greeting
+#with the users name and and inspirational message.
+
+name = input("Hello there, what is your name?")
+
+print (f"Hello, {name}.")
+
+print(f"Sending good vibes your way, {name}.")
