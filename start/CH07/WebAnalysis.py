1d21f67b3407c15e68f995193812a78f93716945
EdenGRC <steves.eden@student.greenriver.edu>
2024-12-03 19:10:44 -0800

    Finished chapter 7 from laptop

--- start/CH07/WebAnalysis.py
+++ start/CH07/WebAnalysis.py
@@ -1,4 +1,5 @@
-#In class assnmt
+#In class assignment
+#I added a function to the code to count restricted access attempts.
 
 import re 
 
@@ -11,16 +12,16 @@ log_file_path = r'C:\Users\piegu\OneDrive\Documents\GitHub\PythonforCybersecurit
 with open(log_file_path, 'r') as file: 
     log_lines = file.readlines()
 
-statuscode_count = defaultdict(int)
+status_code_count = defaultdict(int)
 
 for line in log_lines:
     match = re.search(r'\" \d{3} ', line)
     if match:
         status_code = match.group().strip().split()[1]
-        statuscode_count[status_code] +- 1
+        status_code_count[status_code] += 1
 
-print("Status Code Distbtn: ")
-for code, count in statuscode_count.items():
+print("Status Code Distribution: ")
+for code, count in status_code_count.items():
     print(f"{code}: {count}")
 
 ip_counts = defaultdict(int)
@@ -28,9 +29,21 @@ total_requests = len(log_lines)
 
 for line in log_lines:
     ip_address = line.split()[0]
-    ip_counts[ip_address] +- 1
+    ip_counts[ip_address] += 1
 
 most_frequent_ip = max(ip_counts, key=ip_counts.get)
 percentage = (ip_counts[most_frequent_ip] / total_requests) * 100
 
 print(f"Most frequent IP: {most_frequent_ip} - {percentage:.2f}% of total")
+
+#added functions that count the times restricted pages are attempted to be accessed.
+
+restricted_pages = ['/admin', '/login']
+restricted_page_counts = defaultdict(int)
+for line in log_lines:
+    for page in restricted_pages:
+        if page in line:
+            restricted_page_counts[page] += 1
+
+for page, count in restricted_page_counts.items():
+    print(f"{page}: {count} attempts")
